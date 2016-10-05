ea9221e365858f945505d229a5152bbbf4c6df1e
kamlesh tajpuri <ktajpuri@gmail.com>
2016-10-05 12:29:35 +0000

    sdd

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import App from './App';
+import ReactForm from './ReactForm';
 import './index.css';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
+);
+
+ReactDOM.render(
+  <ReactForm />,
+  document.getElementById('root2')
 );
